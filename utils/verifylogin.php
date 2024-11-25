14d957eb0e4b3d594599f2914d2977ed103f8da6
nasif03 <121179152+nasif03@users.noreply.github.com>
2024-11-25 23:23:02 +0600

    Build login/register backend

--- utils/verifylogin.php
+++ utils/verifylogin.php
@@ -2,5 +2,21 @@
 
 include ('connect_db.php');
 
+if (isset($_POST)) {
+    $phone_no = $_POST['phone-number'];
+    $password = $_POST['password'];
+}
+
+$query_account = "SELECT * FROM farmer WHERE phone_no = '$phone_no' AND password = '$password'";
+$result = mysqli_query($conn, $query_account);
+
+if ($result -> num_rows != 1) {
+    echo "Account does not exist.";
+    header("location: ../login.php");
+} else {
+    
+}
+
+mysqli_close($conn);
 
 ?>
--- utils/verifyregister.php
+++ utils/verifyregister.php
@@ -5,6 +5,7 @@ include ('connect_db.php');
 if (isset($_POST)) {
     $username = $_POST['username'];
     $password = $_POST['password'];
+    $hash = password_hash($password, PASSWORD_DEFAULT);
     $dob = $_POST['dob'];
     $phone_no = $_POST['phone-number'];
     $address = $_POST['address'];
@@ -14,10 +15,10 @@ $query_phone = "SELECT * FROM farmer WHERE phone_no = '$phone_no'";
 $result = mysqli_query($conn, $query_phone);
 
 if ($result -> num_rows > 0) {
-    echo "Phone number already in use.";
+    echo "Phone number is already in use.";
 } else {
     $query_insert = "INSERT INTO farmer(name, password, dob, phone_no, address)
-                     VALUES ('$username', '$password', '$dob', '$phone_no', '$address')";
+                     VALUES ('$username', '$hash', '$dob', '$phone_no', '$address')";
     
     if (mysqli_query($conn, $query_insert)) {
         header("location: ../login.php");
