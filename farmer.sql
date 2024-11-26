90c60070e94810c8e53ea6a97b521362e24ebc34
Shafin <shafinawakes@gmai.com>
2024-11-26 23:53:34 +0600

    additional changes

--- farmer.sql
+++ farmer.sql
@@ -1,8 +1,8 @@
 CREATE TABLE Farmer (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,    
+    farmer_id INT AUTO_INCREMENT PRIMARY KEY,
+    farmer_name VARCHAR(50) NOT NULL,    
     phoneno VARCHAR(15) NOT NULL,    
-    dateofbirth DATE NOT NULL,         
+    DoB DATE NOT NULL,         
     address TEXT NOT NULL,             
-    password VARCHAR(255) NOT NULL     -- Farmer's password (hashed for security)
+    password VARCHAR(255) NOT NULL     
 );
