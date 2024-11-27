58174cfb62e2377741df5fb9b6792bcab2d26864
Shafin <shafinawakes@gmai.com>
2024-11-27 18:43:43 +0600

    Unnecessary thing deleted

--- temporary_frontend_stuff/html/marketplace.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE marketplace (
-    id INT AUTO_INCREMENT PRIMARY KEY,     
-    product_name VARCHAR(100) NOT NULL,    
-    product_category VARCHAR(50) NOT NULL, 
-    unit VARCHAR(20) NOT NULL,             
-    price DECIMAL(10, 2) NOT NULL,         
-    price_change DECIMAL(10, 2) DEFAULT 0
-);
