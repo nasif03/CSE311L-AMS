2d7c8967ed12f43658c67331694a1b0c77ff6633
Shafin <shafinawakes@gmai.com>
2024-11-27 18:36:02 +0600

    Created Crops table

--- Database/Schema/create_tables.sql
+++ Database/Schema/create_tables.sql
@@ -1,6 +1,8 @@
 CREATE DATABASE IF NOT EXISTS agricultural_management_system;
 USE agricultural_management_system;
 
+-- Farmer Table 
+
 DROP TABLE IF EXISTS Farmer;
 
 CREATE TABLE Farmer (
@@ -11,3 +13,16 @@ CREATE TABLE Farmer (
     address TEXT NOT NULL,
     password VARCHAR(255) NOT NULL
 );
+
+
+-- Crops Table 
+
+DROP TABLE IF EXISTS Crops 
+
+CREATE TABLE Crops ( 
+     crop_id INT AUTO_INCREMENT PRIMARY KEY,
+    crop_name VARCHAR(50) NOT NULL,
+    planting_date DATE NOT NULL,
+    soil_type VARCHAR(50) NOT NULL
+); 
+
